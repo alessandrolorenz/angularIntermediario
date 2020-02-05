9e2694fcc037013daebffb46205011b302f3bf01
alessandro lorenz <alessandro.lorenz@compasso.com.br>
2020-02-05 12:14:15 -0300

    concluding ngFor exercicies

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -13,12 +13,14 @@ import {
       MatInputModule,
       MatListModule,
       MatIconModule,
-      MatDividerModule
+      MatDividerModule,
+      MatButtonModule
     } from "@angular/material";
 import { NgClassComponent } from './ng-class/ng-class.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
 import { SubNgIfComponent } from './ng-if/sub-ng-if/sub-ng-if.component';
-import { NgForComponent } from './ng-for/ng-for.component'
+import { NgForComponent } from './ng-for/ng-for.component';
+import { NgForFormComponent } from './ng-for-form/ng-for-form.component'
 
 @NgModule({
   declarations: [
@@ -27,7 +29,8 @@ import { NgForComponent } from './ng-for/ng-for.component'
     NgClassComponent,
     NgIfComponent,
     SubNgIfComponent,
-    NgForComponent
+    NgForComponent,
+    NgForFormComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +43,8 @@ import { NgForComponent } from './ng-for/ng-for.component'
     MatInputModule,
     MatListModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
