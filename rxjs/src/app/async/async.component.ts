3fa5e1ce656e40cd8f4667388a3cab49e53e93ce
alessandro lorenz <alessandro.lorenz@compasso.com.br>
2020-02-10 21:25:42 -0300

    Pipe and async

--- rxjs/src/app/async/async.component.ts
+++ rxjs/src/app/async/async.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, toArray, delay } from 'rxjs/operators';
 
+interface User {
+  login: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-async',
   templateUrl: './async.component.html',
@@ -11,6 +16,7 @@ export class AsyncComponent implements OnInit {
 
   //criar o observable first
   private options$: Observable<string[]>
+  private user$: Observable<User>
 
   constructor() { }
 
@@ -29,7 +35,24 @@ export class AsyncComponent implements OnInit {
       delay(1000)
     );
 
-    this.options$.subscribe(s=>console.log(s))
+    // this.options$.subscribe(s=>console.log(s));
+
+    this.user$ = new Observable<User>((observer)=>{
+      let names = ["Mr. Alceu", "Mr. Pedro", "Mr. Geremias", "Ms. Maria"];
+      let logins = ["Alceu", "Pedro", "Geremias", "Maria"];
+      let i= 0;
+      console.log("Here Observable user$")
+      setInterval(()=>{
+        if(i == 4)
+          observer.complete()
+        else {
+          observer.next({login: logins[i], name: names[i]})
+        }
+        i++
+      }, 2000)
+
+    });
+        // this.user$.subscribe(s=>console.log(s));
      
   }
 
